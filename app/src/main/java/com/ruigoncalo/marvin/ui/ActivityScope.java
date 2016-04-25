b9d832c432d2bc0fda01aeb8200ae009ba56318f
Rui Gonçalo <ruimiguelgoncalo@gmail.com>
2016-04-25 20:55:17 +0100

    Create dagger activity scope

---
+++ app/src/main/java/com/ruigoncalo/marvin/ui/ActivityScope.java
@@ -0,0 +1,14 @@
+package com.ruigoncalo.marvin.ui;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * Created by ruigoncalo on 24/04/16.
+ */
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ActivityScope {
+}
