232b7cc37f987960547e5b046ff2ce46dfcfec76
Rui Gonçalo <ruimiguelgoncalo@gmail.com>
2016-04-30 00:49:19 +0100

    Create errors constants

---
+++ app/src/main/java/com/ruigoncalo/marvin/utils/Errors.java
@@ -0,0 +1,10 @@
+package com.ruigoncalo.marvin.utils;
+
+/**
+ * Created by ruigoncalo on 29/04/16.
+ */
+public class Errors {
+
+    public static final String ERROR_SERVER = "Could not get data from server";
+    public static final String ERROR_INVALID_DATA = "Invalid data";
+}
