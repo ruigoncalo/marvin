8f5acc0273a460147e578f2cbc33baa1e60397a1
Rui Gonçalo <ruimiguelgoncalo@gmail.com>
2016-04-30 12:18:55 +0100

    Add offset parameter to requests

--- app/src/main/java/com/ruigoncalo/marvin/api/Endpoints.java
+++ app/src/main/java/com/ruigoncalo/marvin/api/Endpoints.java
@@ -7,6 +7,7 @@ public class Endpoints {
 
     public static final String PARAM_NAME_STARTS_WITH = "nameStartsWith";
     public static final String PARAM_LIMIT = "limit";
+    public static final String PARAM_OFFSET = "offset";
 
     public static final String BASE_URL = "http://gateway.marvel.com/v1/public/";
     public static final String CHARACTERS = "characters";
